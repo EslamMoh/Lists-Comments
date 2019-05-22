5a200576272bdc0d878cbc86b97407dc72b9e276
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-22 23:23:29 +0200

    add card decorator

---
+++ app/decorators/card_decorator.rb
@@ -0,0 +1,14 @@
+class CardDecorator < Draper::Decorator
+  delegate_all
+
+  def as_json(options = { comments: false })
+    output = {
+      id: id,
+      title: title,
+      description: description
+    }
+
+    output['cards'] = comments.limit(3).try(:decorate).try(:as_json) if options[:comments]
+    output
+  end
+end
