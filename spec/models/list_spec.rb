46aef8b8efa79a9ef3a67346eeb003e3db70b183
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-27 11:44:22 +0200

    add list model spec

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # associations
+  it { should belong_to(:admin).class_name('User') }
+  it { should have_many(:member_lists).dependent(:destroy) }
+  it { should have_many(:members).through(:member_lists).source(:user) }
+  it { should have_many(:cards).dependent(:destroy) }
+  it { should have_many(:comments).through(:cards) }
+
+  # validations
+  it { should validate_presence_of(:title) }
+  it { should validate_uniqueness_of(:title).scoped_to(:admin_id) }
 end
