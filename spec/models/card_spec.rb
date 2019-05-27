74722fe3518c00030b2ef8f85d02e2b270610b8e
Eslam Mohamed <eslammoh.ce@gmail.com>
2019-05-27 13:45:38 +0200

    add card model spec

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # associations test
+  it { should have_many(:comments).dependent(:destroy) }
+  it { should belong_to(:user) }
+  it { should belong_to(:list) }
+
+  # validations test
+  %i[title description].each do |field|
+    it { should validate_presence_of(field) }
+  end
+  it { should validate_uniqueness_of(:title).scoped_to(:list_id) }
 end
